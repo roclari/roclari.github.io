743b5d790800ea9d5d67161b623c36a5c1738690
roclari <98750360+roclari@users.noreply.github.com>
2022-11-14 20:48:19 -0300

    portfolio

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+function mudaModo() {
+    document.body.classList.toggle("dark");
+  }
+
+
+const slidesContainer = document.getElementById("slides-container");
+const slide = document.querySelector(".slide");
+const prevButton = document.getElementById("slide-arrow-prev");
+const nextButton = document.getElementById("slide-arrow-next");
+
+nextButton.addEventListener("click", () => {
+    const slideWidth = slide.clientWidth;
+    slidesContainer.scrollLeft += slideWidth;
+});
+
+prevButton.addEventListener("click", () => {
+    const slideWidth = slide.clientWidth;
+    slidesContainer.scrollLeft -= slideWidth;
+});
